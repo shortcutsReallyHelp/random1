0f6b9eee3d037508e00240fa1c62881c76a29dc4
ngmanov <user@user.user>
2021-10-15 19:23:57 +0600

    feat: adds comment

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Str;
 
 class GameController extends Controller
 {
+    //пока не тестил
     public function index()
     {
         return MatchResource::collection(Match::query()
