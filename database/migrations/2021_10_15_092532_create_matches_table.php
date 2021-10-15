bfd9e780808d27b8b3f95d0588471f9e7f530644
ngmanov <user@user.user>
2021-10-15 15:30:50 +0600

    feat: creates teams, matches tables

---
+++ database/migrations/2021_10_15_092532_create_matches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('left_team_id')->references('id')->on('teams');
+            $table->foreignId('right_team_id')->references('id')->on('teams');
+            $table->integer('left_team_score');
+            $table->integer('right_team_score');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
